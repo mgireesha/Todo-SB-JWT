0ad793edae49bcee268169182b25470e4a7c6a91
mgireesha <mgireesha77@gmail.com>
2023-03-11 23:49:55 +0530

    Update send email service details, Fixed height issue for mobile

---
+++ src/main/java/com/gmt/todo/utils/TODO_CONSTANTS.java
@@ -0,0 +1,6 @@
+package com.gmt.todo.utils;
+
+public class TODO_CONSTANTS {
+    public static final String EMAIL_SERVICE_URI = "https://sendemail-mgireesha77.b4a.run/sendEmail";
+    public static final String EMAIL_SERVICE_J_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBZG1pbiIsImV4cCI6MTY3OTQ0NDU1NCwiaWF0IjoxNjc4NTU1NTIxfQ.3yD5ldNBWB120--R35LtQFIvuV6HuaP-aRhk6BxUJY4";
+}
