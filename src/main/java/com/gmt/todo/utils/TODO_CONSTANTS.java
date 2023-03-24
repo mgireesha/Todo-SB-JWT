52941efa4c6d79f89c0cdc6fdf88a50efa1c2f2d
mgireesha <mgireesha77@gmail.com>
2023-03-24 23:07:30 +0530

    Update email service token

--- src/main/java/com/gmt/todo/utils/TODO_CONSTANTS.java
+++ src/main/java/com/gmt/todo/utils/TODO_CONSTANTS.java
@@ -6,5 +6,5 @@ public class TODO_CONSTANTS {
     public static final String SUCCESS = "SUCCESS";
 
     public static final String EMAIL_SERVICE_URI = "https://sendemail-mgireesha77.b4a.run/sendEmail";
-    public static final String EMAIL_SERVICE_J_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBZG1pbiIsImV4cCI6MTY3OTQ0NDU1NCwiaWF0IjoxNjc4NTU1NTIxfQ.3yD5ldNBWB120--R35LtQFIvuV6HuaP-aRhk6BxUJY4";
+    public static final String EMAIL_SERVICE_J_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBZG1pbiIsImV4cCI6MTY4MDU2Nzg4MiwiaWF0IjoxNjc5Njc4ODQ5fQ.DCQ_MqJnK1K7flFmas7ygsmtmad2PaU_FZnNBSCbkaI";
 }
